75ad9ca9431d3325e6fd75d180d065402b519e30
Scott Griffin <mscottgriffin@gmail.com>
2020-07-07 13:27:55 -0400

    starting functions

--- greatbay.js
+++ greatbay.js
@@ -1,21 +1,38 @@
-var mysql = require("mysql");
+const mysql = require('mysql');
+const inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: 'localhost',
 
   // Your port; if not 3306
   port: 3306,
 
   // Your username
-  user: "root",
+  user: 'root',
 
   // Your password
-  password: "",
-  database: " " // project_db
+  password: '',
+  database: 'great_bay_DB', // project_db
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
+  console.log('connected as id ' + connection.threadId);
   connection.end();
 });
+
+//bid or post function
+const postOrBid = () => {
+  inquirer.prompt([
+
+  ])};
+
+//create auction function
+const createAuction = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What item are you auctioning?',
+    },
+  ])};
